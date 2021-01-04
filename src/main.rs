70cd9fa25104588f7a0fba765cb741eae27e886a
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2021-01-04 14:23:19 -0500

    Switched from diesel to sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-	println!("hello");
+ use juniper::{
+	graphql_object, EmptyMutation, EmptySubscription, FieldResult, 
+	GraphQLEnum, Variables, GraphQLObject,
+ };
+
+ pub struct Query;
+ mod schedule;
+
+// A root schema consists of a query, a mutation, and a subscription.
+// Request queries can be executed against a RootNode.
+// pub type Schema = juniper::RootNode<'static, Query, EmptyMutation, EmptySubscription>;
+
+type Schema = juniper::RootNode<'static, Query, EmptyMutation<()>, EmptySubscription<()>>;
+
+async fn run() {
+	let (res, errors) = juniper::execute(
+		"query {schedule{ start }}",
+		None,
+		&Schema::new(Query, EmptyMutation::new(), EmptySubscription::new()),
+		&Variables::new(),
+		&()
+	).await.unwrap();
+
+	println!("----");
+	println!("{}", res);
+	println!("----");
+}
+
+use reqwest::get;
+
+#[async_std::main]
+async fn main() -> Result<(), sqlx::Error> {
+
+	let result = get("");
+
+	run().await;
+	Ok(())
 }
--- src/schedule.rs
+++ src/schedule.rs
@@ -1,110 +1,119 @@
-use diesel::prelude::*;
-// use diesel::sql_types;
-use diesel::pg::PgConnection;
-
-use juniper::{
+ use juniper::{
 	graphql_object, EmptyMutation, EmptySubscription, FieldResult, 
 	GraphQLEnum, Variables, GraphQLObject,
-};
+ };
 
-table! {
-	mounts {
-		id -> Integer,
-		shortname -> VarChar,
-		password -> VarChar,
-		mountpoint -> VarChar,
+
+use::sqlx::postgres::PgPoolOptions;
+
+// use std::str::FromStr;
+
+#[derive(sqlx::FromRow)]
+#[derive(GraphQLObject)]
+#[derive(Clone)]
+struct MountPoint{
+	id: i32,
+	shortname: String,
+	password: String,
+	mountpoint: String,
+}
+
+use sqlx::postgres::types::*;
+use chrono::DateTime;
+use chrono::offset::Utc;
+
+// type alias for postgresql timerange
+type TimeRange = PgRange<DateTime<Utc>>;
+
+// #[derive(GraphQLObject)]
+#[derive(sqlx::FromRow)]
+struct TimeSlot{
+	id: i32,
+	shortname: String,
+	time_range: TimeRange,
+}
+
+
+use std::ops::Bound::*;
+
+#[graphql_object]
+impl TimeSlot {
+	/// the shortname of the show
+	fn show(&self) -> &str {
+		&self.shortname	
+	}
+
+	/// the show's start time
+	fn start(&self) -> DateTime<Utc> {
+		match self.time_range.start {
+			Included(time) => time,
+			Excluded(time) => time,
+			Unbounded => panic!(),
+		}
+	}
+	
+	/// the show's end time
+	fn end(&self) -> DateTime<Utc> {
+		match self.time_range.end {
+			Included(time) => time,
+			Excluded(time) => time,
+			Unbounded => panic!(),
+		}
 	}
 }
-
-table!{
-	// use diesel::sql_types::*;
-	schedule {
-		id -> Integer,
-		shortname -> VarChar,
-		time_range -> VarChar,
-		year -> VarChar,
-		season -> VarChar,
-	}
-}
-
-#[derive(GraphQLObject)]
-#[derive(Queryable)]
-pub struct Mount {
-	pub id: i32,
-	pub shortname: String,
-	pub mountpoint: String,
-	pub password: String,
-}
-
-#[derive(GraphQLObject)]
-#[derive(Queryable)]
-pub struct Timeslot {
-	pub id: i32,
-	pub shortname: String,
-	pub timerange: String,
-	pub year: String,
-	pub season: String,
-}
-
-
-// database functions
-pub fn establish_connection() -> PgConnection {
-	let database_url = "postgres://dev:hackme@api.wjrh.org:5432/graphqltest";
-	PgConnection::establish(&database_url)
-		.expect(&format!("Error connecting to {}", database_url))
-}
-
-pub fn get_mounts(connection: PgConnection) -> Vec<Mount>{
-	use crate::schedule::mounts::dsl::*;
-	let results = mounts
-		.load::<Mount>(&connection)
-		.expect("error");
-
-	return results;
-}
-
-pub fn get_schedule(conn: PgConnection) -> Vec<Timeslot>{
-	use crate::schedule::schedule::dsl::*;
-	return schedule.load::<Timeslot>(&conn)
-		.expect("error")
-}
-
-// pub struct Query;
 
 use super::Query;
 #[graphql_object()]
 impl Query {
-	fn example_mount(&self) -> Mount{
-		Mount{
-			id: 1,	
-			shortname: "example".to_string(),
-			mountpoint: "example".to_string(),
-			password: "ABCDE".to_string(),
-		}
+	async fn mountpoint(shortname: String) -> MountPoint {
+		let database_url = "postgres://dev:hackme@api.wjrh.org:5432/graphqltest";
+	
+		let pool = PgPoolOptions::new()
+			.max_connections(5)
+			.connect(database_url)
+			.await
+			.unwrap();
+	
+		let result = sqlx::query_as::<_, MountPoint>("select * from mounts where shortname=$1;")
+			.bind(shortname)
+			.fetch_one(&pool)
+			.await
+			.unwrap();
+
+		return result
 	}
 
-	fn mounts(&self) -> Vec<Mount> {
-		let connection = establish_connection();
-		let mounts = get_mounts(connection);
+	async fn schedule() -> Vec<TimeSlot> {
+		let database_url = "postgres://dev:hackme@api.wjrh.org:5432/graphqltest";
+		let pool = PgPoolOptions::new()
+			.max_connections(5)
+			.connect(database_url)
+			.await
+			.unwrap();
 
-		return mounts;
+		let result = sqlx::query_as::<_, TimeSlot>("select * from schedule")
+			.bind("")
+			.fetch_all(&pool)
+			.await
+			.unwrap();
+
+		return result
 	}
 }
 
 
-#[cfg(test)]
-mod tests {
-	use super::*;
+// #[cfg(test)]
+// mod tests {
+//	use super::*;
 
-	/// test that a connection to Postgres can be established
-	#[test]
-	fn test_connect() {
-		establish_connection();
-	}
-
-	#[test]
-	fn test_schedule(){
-		let conn = establish_connection();
-		get_schedule(conn);
-	}
-}
+//	#[test]	
+//	fn test_1() {
+//		let (res, errors) = juniper::execute(
+//			"query {example {shortname}}",
+//			None,
+//			&Schema::new(Query, EmptyMutation::new(), EmptySubscription::new()),
+//			&Variables::new(),
+//			&()
+//		).unwrap();
+//	}
+// }
