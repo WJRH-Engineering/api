f35ff7a17525eeac2b7394d94c39cc2482ee979e
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2021-04-11 15:22:57 -0400

    more refactoring of schedule file
    Schedule is now its own type, and contains the resolver for fetching its
    list of timeslots, as well as extra metadata.
    
    Seasons are no longer kept as strings, but instead as their own Season
    enum, which can be either one of the most common types: SPRING, FALL,
    SUMMER, or Other, which can represent any other string. The season type
    seamlessly converts to and from Strings, and its purpose is mainly to
    help rust optimize the code and to make the Schedule type more explicit.
    
    Began adding doc comments

--- src/graphql_types/schedule.rs
+++ src/graphql_types/schedule.rs
@@ -3,13 +3,64 @@ use crate::data_sources::postgres;
 use core::ops::Bound::*;
 use crate::graphql_types::teal;
 use async_graphql::*;
+use std::fmt;
 
 
-#[derive(Debug, SimpleObject)]
-struct ScheduleID {
+#[derive(Debug)]
+struct Schedule {
     year: i32,
-    season: String,
+    season: Season,
 }
+
+/// ## Schedule
+/// 
+/// The 'Schedule' type represents WJRH's weekly programming schedule for a
+/// given year and season. 
+#[Object]
+impl Schedule {
+
+    /// Schedules are uniquely identified by a combination of a year and a 
+    /// season. Season's are typically either FALL or SPRING, although
+    /// potentially any value could be used.
+    ///
+    /// Schedule ids will be formatted "{year} {season}"
+    /// eg. 2020 FALL, 2021 SPRING, 2022 SUMMER, etc.
+    ///
+    /// The year and season can also be accessed individually through the 
+    /// `year` and `season` fields.
+    pub async fn id(&self) -> String {
+        self.to_string()
+    } 
+
+    /// Schedules are defined by a set of timeslots. Timeslots contain a 
+    /// `Program` as well as a weekly start and end time. These timeslots
+    /// represent both remote shows and shows done in the studio.
+    pub async fn timeslots(&self) -> Vec<TimeSlot> {
+       let season = self.season.to_string();
+       let timeslots = postgres::query_schedule(self.year, &season);
+       let schedule: TimeSlots = timeslots.await.into();
+       schedule.timeslots()
+    }
+
+    pub async fn year(&self) -> i32 { self.year }
+    pub async fn season(&self) -> String { self.season.to_string() }
+}
+
+impl Schedule { 
+    fn new(year: i32, season_str: &str) -> Self {
+        Self { year, season: season_str.into() }
+    }
+}
+
+impl fmt::Display for Schedule { 
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.year, self.season)
+    }
+}
+
+// --------
+// TIMESLOT
+// --------
 
 #[derive(Debug, SimpleObject)]
 #[graphql(complex)]
@@ -22,8 +73,7 @@ struct TimeSlot {
     #[graphql(skip)]
     pub end: TimeOfWeek,
 
-    #[graphql(skip)]
-    pub schedule: ScheduleID,
+    pub schedule: Schedule,
 }
 
 
@@ -36,28 +86,62 @@ impl TimeSlot {
     }
 }
 
+// -----
+// QUERY
+// -----
 
 #[derive(Default)]
 pub struct ScheduleQuery;
 
 #[Object]
 impl ScheduleQuery {
-    pub async fn schedule(&self) -> Vec<TimeSlot> {
-       let schedule: Schedule = postgres::query_schedule(2021, "SPRING")
-           .await.into();
-       schedule.timeslots()
+    pub async fn schedule(&self) -> Schedule {
+        Schedule::new(2021, "SPRING")
     }
 }
 
 use core::ops::Bound;
 
-/// Unwraps a Bound enum, returning the value it wraps if there is one,
-/// and panicing if it is Unbounded
-fn unwrap_bound<T>(bound: Bound<T>) -> T {
-    match bound {
-        Included(inner) => inner,
-        Excluded(inner) => inner,
-        Unbounded => panic!("cannot unwrap unbounded"),
+
+// -----------
+// SEASON ENUM
+// -----------
+
+/// An ENUM representing the different seasons that a schedule can take place
+/// in. In theory, this could be any String, but in practice the vast majority
+/// will correspond to one of Lafayette's semesters, which is typically how
+/// long the station will run shows for.
+///
+/// The Season type implements Display and From<&str> so that it can be easily
+/// converted to and from strings.
+#[derive(Debug)]
+enum Season {
+    Spring,
+    Fall,
+    Summer,
+    Other(String),
+}
+
+impl From<&str> for Season {
+   fn from(string: &str) -> Self {
+       match string.to_uppercase().as_str() { 
+            "SPRING" => Self::Spring,
+            "FALL" => Self::Fall,
+            "SUMMER" => Self::Summer,
+            _ => Self::Other(string.to_string()),
+       }
+   } 
+}
+
+impl fmt::Display for Season {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let out = match self {
+            Self::Spring => "SPRING",
+            Self::Fall => "FALL",
+            Self::Summer => "SUMMER",
+            Self::Other(val) => val,
+        };
+        write!(f, "{}", out)
     }
 }
 
@@ -72,37 +156,46 @@ impl From<postgres::TimeSlot> for TimeSlot {
         //convert time_range
         let start: TimeOfWeek = unwrap_bound(timeslot.time_range.start).into();
         let end: TimeOfWeek = unwrap_bound(timeslot.time_range.end).into();
+        let schedule = Schedule::new(timeslot.year, &timeslot.season);
 
-        let schedule = ScheduleID{
-            season: timeslot.season,
-            year: timeslot.year,
-        };  
         let shortname = timeslot.shortname;
 
         Self { shortname, start, end, schedule }
     }
 }
 
+/// Unwraps a Bound enum, returning the value it wraps if there is one,
+/// and panicing if it is Unbounded
+fn unwrap_bound<T>(bound: Bound<T>) -> T {
+    match bound {
+        Included(inner) => inner,
+        Excluded(inner) => inner,
+        Unbounded => panic!("cannot unwrap unbounded"), }
+}
+
 /// A wrapper type around a vector of timeslots to make operating on them 
 /// cleaner. Using a wrapper type like this allows us to implement the From
 /// trait.
-struct Schedule(Vec<TimeSlot>);
-impl Schedule {
+struct TimeSlots(Vec<TimeSlot>);
+impl TimeSlots {
     fn timeslots(self) -> Vec<TimeSlot> {
         self.0
     }
 }
 
-impl From<Vec<postgres::TimeSlot>> for Schedule {
+impl From<Vec<postgres::TimeSlot>> for TimeSlots {
     fn from(timeslots: Vec<postgres::TimeSlot>) -> Self {
         let mut output: Vec<TimeSlot> = vec![];
         for timeslot in timeslots {
             output.push(timeslot.into());
         }
-        Schedule(output) 
+        Self(output) 
     }
 }
 
+// ----------
+// UNIT TESTS
+// ----------
 
 #[cfg(test)]
 mod tests{
